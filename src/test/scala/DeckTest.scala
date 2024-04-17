8631fdb8aac951b5f5877a7568842fca0f51d300
Philippe Westenfelder <philippe.westenfelder@htwg-konstanz.de>
2024-04-17 18:08:48 +0200

    init tests

---
+++ src/test/scala/DeckTest.scala
@@ -0,0 +1,23 @@
+//import org.scalatest.*
+import org.scalatest.wordspec.AnyWordSpec
+
+class DeckTest extends  AnyWordSpec{
+  "Deck" should {
+    "have 61 cards" in {
+      assert(deck.cards.length == 61)
+    }
+    "have 56 cards of each suit" in {
+      assert(deck.cards.count(_.suit == Suit.Red) == 14)
+      assert(deck.cards.count(_.suit == Suit.Black) == 14)
+      assert(deck.cards.count(_.suit == Suit.Blue) == 14)
+      assert(deck.cards.count(_.suit == Suit.Yellow) == 14)
+    }
+    "have 5 special cards" in {
+      assert(deck.cards.count(card => card.suit == SpecialCard.Joker) == 1)
+      assert(deck.cards.count(card => card.suit == SpecialCard.Mermaid) == 1)
+      assert(deck.cards.count(card => card.suit == SpecialCard.SkullKing) == 1)
+      assert(deck.cards.count(card => card.suit == SpecialCard.Pirate) == 1)
+      assert(deck.cards.count(card => card.suit == SpecialCard.Escape) == 1)
+    }
+  }
+}

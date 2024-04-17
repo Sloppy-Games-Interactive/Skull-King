402ed0b20f975cd863711135eaf1703ad0a1f527
Philippe Westenfelder <philippe.westenfelder@htwg-konstanz.de>
2024-04-17 18:48:04 +0200

    first try

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,25 @@
+import scala.annotation.tailrec
+import scala.io.StdIn.readLine
+
 @main def run(): Unit =
-  println("Yo ho ho ho!")
-  println("Hallo!")
-  println(s"played cards: $deck")
+  val numPlayers = askHowManyPlayers()
+  
+  
+  
+@tailrec
+def askHowManyPlayers(): Int = {
+  val minPlayers = 2
+  val maxPlayers = 4
+  
+  print("How many players? ")
+  val numPlayers = readLine().toInt
+  if (numPlayers < minPlayers || numPlayers > maxPlayers) {
+    println(s"Please enter a number between $minPlayers and $maxPlayers.")
+    askHowManyPlayers()
+  } else {
+    numPlayers
+  }
+}
 
 case class Deck(cards:List[Card])
 val deck: Deck = Deck((redCards ++ blueCards ++ yellowCards ++ blackCards ++ specialCards).toList)
--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,1 +1,11 @@
+import org.scalatest.wordspec.AnyWordSpec
 
+class MainTest extends AnyWordSpec {
+  "Main" should {
+    "know how many players are in the game" in {
+      val players = askHowManyPlayers()
+      print(2)
+      assert(players == 2)
+    }
+  }
+}

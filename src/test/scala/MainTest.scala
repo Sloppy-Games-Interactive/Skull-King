f83718978b2e8967e70b752cd5c9578b6a444e75
Philippe Westenfelder <philippe.westenfelder@htwg-konstanz.de>
2024-04-18 16:33:33 +0200

    add main tests

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,11 +1,40 @@
 import org.scalatest.wordspec.AnyWordSpec
 
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, PrintStream}
+import java.nio.charset.StandardCharsets
+
 class MainTest extends AnyWordSpec {
-  "Main" should {
-    "know how many players are in the game" in {
-      val players = askHowManyPlayers()
-      print(2)
-      assert(players == 2)
+  "askHowManyPlayers" should {
+    "return the correct number of players" in {
+      val input = new ByteArrayInputStream("3\n".getBytes(StandardCharsets.UTF_8))
+      val output = new ByteArrayOutputStream()
+      Console.withIn(input) {
+        Console.withOut(new PrintStream(output)) {
+          assert(askHowManyPlayers() == 3)
+        }
+      }
+      assert(output.toString(StandardCharsets.UTF_8.name()).trim == "How many players?")
+    }
+
+    "throw an exception for invalid input" in {
+      val input = new ByteArrayInputStream("invalid\n".getBytes(StandardCharsets.UTF_8))
+      Console.withIn(input) {
+        assertThrows[NumberFormatException] {
+          askHowManyPlayers()
+        }
+      }
+    }
+
+    "ask again if the number of players is too small" in {
+      val input = new ByteArrayInputStream("1\n3\n".getBytes(StandardCharsets.UTF_8))
+      val output = new ByteArrayOutputStream()
+      Console.withIn(input) {
+        Console.withOut(new PrintStream(output)) {
+          assert(askHowManyPlayers() == 3)
+        }
+      }
+      val expectedOutput = "How many players? Please enter a number between 2 and 4.\nHow many players?"
+      assert(output.toString(StandardCharsets.UTF_8.name()).trim == expectedOutput)
     }
   }
 }

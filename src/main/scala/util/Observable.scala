00dc8cbf3b4e38ec678a1704859c4c9d0f2c95ad
juu <nohl.julian@gmail.com>
2024-05-03 09:09:27 +0200

    unit

--- src/main/scala/util/Observable.scala
+++ src/main/scala/util/Observable.scala
@@ -8,7 +8,7 @@ class ObservableEvent {}
 
 class Observable {
   var subscribers:Vector[Observer] = Vector()
-  def add(s:Observer) = subscribers = subscribers :+ s
-  def remove(s:Observer) = subscribers = subscribers.filterNot(o=>o==s)
-  def notifyObservers(e:ObservableEvent = ObservableEvent()) = subscribers.foreach(o => o.update(e))
+  def add(s:Observer): Unit = subscribers = subscribers :+ s
+  def remove(s:Observer): Unit = subscribers = subscribers.filterNot(o=>o==s)
+  def notifyObservers(e:ObservableEvent = ObservableEvent()): Unit = subscribers.foreach(o => o.update(e))
 }

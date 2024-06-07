c8bf8661a420ef591696bc43db024a4b06df54e0
j.nohl <nohl.julian@gmail.com>
2024-06-07 02:39:06 +0200

    add micro game button

--- src/main/scala/de/htwg/se/skullking/view/gui/components/GameButton.scala
+++ src/main/scala/de/htwg/se/skullking/view/gui/components/GameButton.scala
@@ -4,10 +4,11 @@ import de.htwg.se.skullking.view.gui.Styles
 import scalafx.scene.control.Button
 
 enum BtnSize:
-  case small, medium, large
+  case micro, small, medium, large
 
 class GameButton(size: BtnSize) extends Button {
   size match {
+    case BtnSize.micro => this.setId("micro-button")
     case BtnSize.small => this.setId("small-button")
     case BtnSize.medium => this.setId("medium-button")
     case BtnSize.large => this.setId("large-button")
